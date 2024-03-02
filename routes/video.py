b1686facaa4018450a6c4ef1756d0bdfe4f610f9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-02 23:00:29 +0530

    Update video.py

--- routes/video.py
+++ routes/video.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 router = APIRouter(
     prefix="/api/video",
     tags=["video"],
-    responses={404: {"description": "The requested page was not found"}},
+    responses={404: {"description": "The requested uri was not found"}},
 )
