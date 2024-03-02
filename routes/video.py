f51968a199d2510cb94a68643568df6fcebd186f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-02 23:00:20 +0530

    Update video.py

--- routes/video.py
+++ routes/video.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 router = APIRouter(
-    prefix="/api/auth",
+    prefix="/api/video",
     tags=["auth"],
     responses={404: {"description": "The requested page was not found"}},
 )
