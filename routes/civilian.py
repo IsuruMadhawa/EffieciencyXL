21f0cef7ebb685a56bed93c227b27359d5d679a6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-12 02:33:58 +0530

    Update civilian.py

--- routes/civilian.py
+++ routes/civilian.py
@@ -10,6 +10,36 @@ router = APIRouter(
 
 
 @router.post("/lost-item-report")
+async def lost_item_report(
+        name: str = Form(...),
+        division: str = Form(...),
+        district: str = Form(...),
+        description: str = Form(...),
+        token: str = Depends(oauth2_scheme)
+):
+    """
+    The endpoint for reporting lost items
+    Args:
+        name: name of the civilian
+        division: police division
+        district: district
+        description: description of the lost item
+        token: access token
+
+    Returns:
+
+    """
+    user = await get_current_user(token)
+
+    if user is None:
+        raise credentials_exception
+
+    if user.role != "civilian":
+        return {"message": "Only civilians can report lost items"}
+
+    # TODO Add the report to the database
+
+    return {"message": "Lost item report is sent"}
 
 
 @router.post("/request-clearance")
