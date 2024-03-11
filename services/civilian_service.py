dcbb156785042cddd2bf2def995f74d7e79aea96
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-12 02:36:41 +0530

    Update civilian_service.py

--- services/civilian_service.py
+++ services/civilian_service.py
@@ -1,3 +1,17 @@
 from services.database_service import dao
 
 
+def clearance_request(name, division, district, description):
+    """
+    Add a clearance request to the database
+    Args:
+        name: name of the civilian
+        division: police division
+        district: district
+        description: description of the request
+
+    Returns:
+        "Request for clearance is sent" if the request was inserted successfully
+        An error message if the request was not inserted successfully
+    """
+    return dao.insert_clearance_request(name, division, district, description)
