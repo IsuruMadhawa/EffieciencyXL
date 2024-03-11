b3158333b90c73030ba793f91e3f940267344a8c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-12 02:31:42 +0530

    Update civilian.py

--- routes/civilian.py
+++ routes/civilian.py
@@ -14,6 +14,7 @@ async def request_clearance(
         name: str = Form(...),
         division: str = Form(...),
         district: str = Form(...),
+        description: str = Form(...),
         token: str = Depends(oauth2_scheme)
 ):
     """
