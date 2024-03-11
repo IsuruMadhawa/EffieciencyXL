4cba01ba3ffd9f3d3c270fd23b8faa32e440f83a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-12 02:39:05 +0530

    Update civilian.py

--- routes/civilian.py
+++ routes/civilian.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Form
 
 from auth.authorize import get_current_user, credentials_exception, oauth2_scheme
-from services.civilian_service import clearance_request
+from services.civilian_service import clearance_request, add_lost_item_report
 
 router = APIRouter(
     prefix="/api/civilian",
