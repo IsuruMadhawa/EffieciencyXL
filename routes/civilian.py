30faefa30bb42487aac9d9cabe0b08b952d427bc
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-12 02:42:01 +0530

    Update civilian.py

--- routes/civilian.py
+++ routes/civilian.py
@@ -79,4 +79,6 @@ async def request_clearance(
     # Add the request to the database
     clearance_request(name, division, district, description)
 
+    # TODO: Send a notification to the divisional officer
+
     return {"message": "Request for clearance is sent"}
