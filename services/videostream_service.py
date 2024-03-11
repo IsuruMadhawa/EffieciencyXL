e69fa16d8de9b8775ddbce47958d13b49c8d6a63
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-12 01:47:07 +0530

    Update videostream_service.py

--- services/videostream_service.py
+++ services/videostream_service.py
@@ -1,5 +1,6 @@
 from services.database_service import dao
 
 
-def add_video_data(label, location, timestamp, misc):
+def add_video_data(label, location, misc):
+    timestamp =
     return dao.insert_video_data(label, location, timestamp, misc)
