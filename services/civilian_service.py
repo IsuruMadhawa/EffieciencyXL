c17f9a35974cb4674cd3a318ad131026088c2928
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-12 02:36:50 +0530

    Update civilian_service.py

--- services/civilian_service.py
+++ services/civilian_service.py
@@ -15,3 +15,20 @@ def clearance_request(name, division, district, description):
         An error message if the request was not inserted successfully
     """
     return dao.insert_clearance_request(name, division, district, description)
+
+
+def lost_item_report(name, division, district, item, description):
+    """
+    Add a lost item report to the database
+    Args:
+        name: name of the civilian
+        division: police division
+        district: district
+        item: the lost item
+        description: description of the lost item
+
+    Returns:
+        "Lost item report is sent" if the report was inserted successfully
+        An error message if the report was not inserted successfully
+    """
+    return dao.insert_lost_item_report(name, division, district, item, description)
