2d9be0360c3cd60021eecc9d24509dd5a16163c6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-11 22:36:09 +0530

    Update face_rec.py

--- ML/face_rec.py
+++ ML/face_rec.py
@@ -23,3 +23,28 @@ def compute_face_descriptors(training_dataset_path):
                 face_labels.append(filename)  # Use filename as label
 
     return face_descriptors, face_labels
+
+
+def recognize_face(face_descriptors, face_labels, unknown_face_path, threshold=0.6):
+    detector = dlib.get_frontal_face_detector()
+    sp = dlib.shape_predictor("shape_predictor_5_face_landmarks.dat")
+    facerec = dlib.face_recognition_model_v1("dlib_face_recognition_resnet_model_v1.dat")
+
+    img = dlib.load_rgb_image(unknown_face_path)
+    detections = detector(img, 1)
+    if len(detections) > 0:
+        shape = sp(img, detections[0])
+        face_descriptor = facerec.compute_face_descriptor(img, shape)
+
+        distances = [np.linalg.norm(face_descriptor - train_descriptor) for train_descriptor in face_descriptors]
+        closest_index = np.argmin(distances)
+        min_distance = distances[closest_index]
+
+        if min_distance > threshold:
+            return "Not identified"
+        else:
+            label = face_labels[closest_index]
+            return label
+
+    else:
+        raise ValueError("No face detected in the image.")
