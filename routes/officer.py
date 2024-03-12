ef491ebe9ee4aa012769404192a4c55539040da6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-12 17:56:32 +0530

    Update officer.py

--- routes/officer.py
+++ routes/officer.py
@@ -57,7 +57,7 @@ async def approve_clearance_report(
     report = get_single_clearance_request(r_id)
 
     #  TODO timestamp, officer id, details
-    
+    update_clearance_report(report)
 
     # notify the civilian
     user_email = report.c_email
