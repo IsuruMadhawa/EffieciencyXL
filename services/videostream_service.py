044c8a8505c6c33be37fc7c04742e18b816869c7
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-12 17:26:01 +0530

    Update videostream_service.py

--- services/videostream_service.py
+++ services/videostream_service.py
@@ -1,8 +1,8 @@
 from services.database_service import dao
 
 
-def add_video_data(label, location, misc):
-    return dao.insert_video_data(label, location, misc)
+def add_video_data(label, location, misc, img):
+    return dao.insert_video_data(label, location, misc, img)
 
 
 def get_feeddata_for_name(name: str):
