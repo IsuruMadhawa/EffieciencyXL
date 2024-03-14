9aa5891d5956dcfff2b1cd93569e6b476ad99510
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-14 21:02:42 +0530

    Update officer_service.py

--- services/officer_service.py
+++ services/officer_service.py
@@ -4,7 +4,8 @@ from services.database_service import dao
 def get_all_clearance_requests():
     return dao.query_all_clearance_requests()
 
-
+def get_all_complaints():
+    return dao.query_all_complaints()
 
 
 def get_all_lost_item_reports():
