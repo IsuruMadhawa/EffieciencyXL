99575181b590c853dc37c711fb0d2cc25d1eafe3
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-14 18:27:06 +0530

    Update civilian_service.py

--- services/civilian_service.py
+++ services/civilian_service.py
@@ -1,20 +1,20 @@
 from services.database_service import dao
 
 
-def complaint(name, division, district, complaint):
+def add_complaint(name, division, district, complaint_txt):
     """
     Add a complaint to the database
     Args:
         name: name of the civilian
         division: police division
         district: district
-        complaint: the complaint
+        complaint_txt: the complaint
 
     Returns:
         "Complaint is sent" if the complaint was inserted successfully
         An error message if the complaint was not inserted successfully
     """
-    return dao.insert_complaint(name, division, district, complaint)
+    return dao.insert_complaint(name, division, district, complaint_txt)
 
 
 def clearance_request(name, division, district, description):
