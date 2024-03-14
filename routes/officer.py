ff977210d3cd5e4afca9c547982babf70865f4cf
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-03-14 20:59:12 +0530

    Update officer.py

--- routes/officer.py
+++ routes/officer.py
@@ -42,6 +42,37 @@ async def request_clearance(
     return get_all_clearance_requests()
 
 
+@router.post("/disapprove-clearance-report")
+async def disapprove_clearance_report(
+        r_id: int = Form(...),
+        details: str = Form(...),
+        token: str = Depends(oauth2_scheme)
+):
+    user = await get_current_user(token)
+
+    if user is None:
+        raise credentials_exception
+
+    if user.role != "officer":
+        return {"message": "Only officers can disapprove clearance reports"}
+
+    report = get_single_clearance_request(r_id)
+    print(report[0][7])
+    o_id = get_officer_id(user.id)
+
+    result = update_clearance_report(r_id, o_id, details)
+
+    # TODO Uncomment this
+    # notify the civilian
+    # user_email = report[0][7]
+    # send_mail(
+    #     user_email,
+    #     "Your clearance report has been disapproved"
+    # )
+
+    return result
+
+
 @router.post("/approve-clearance-report")
 async def approve_clearance_report(
         r_id: int = Form(...),
